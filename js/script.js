5b8f1bb6d51827e310dc0510fa2442600e92a77f
hamza <hamza786504@gmail.com>
2022-07-16 21:10:37 +0500

    This was the first revision from fiverr client

--- js/script.js
+++ js/script.js
@@ -23,21 +23,22 @@ const login_modelbox = document.getElementById("login_modelbox");
 const close_login_modelbox = document.getElementById("close_login_modelbox");
 const we_prasent_btn = document.getElementById("we_prasent_btn");
 
+
+const modelbox_back = document.getElementById("modelbox_back");
+
+
 we_prasent_btn.addEventListener("click" , function(){
-    login_modelbox.classList.remove("d-none");
-    login_modelbox.classList.add("d-block");
+    modelbox_back.classList.remove("d-none");
+    modelbox_back.classList.add("d-block");
 })
 herosection_btn.addEventListener("click" , function(){
-    login_modelbox.classList.remove("d-none");
-    login_modelbox.classList.add("d-block");
+    modelbox_back.classList.remove("d-none");
+    modelbox_back.classList.add("d-block");
 })
+
+
 
 close_login_modelbox.addEventListener("click" , function(){
-    login_modelbox.classList.remove("d-block");
-    login_modelbox.classList.add("d-none");
-})
-
-login_modelbox.addEventListener("click" , function(){
-    login_modelbox.classList.remove("d-block");
-    login_modelbox.classList.add("d-none");
+    modelbox_back.classList.remove("d-block");
+    modelbox_back.classList.add("d-none");
 })
